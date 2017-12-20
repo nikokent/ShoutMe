f55258460688abe945b8940728c2cfaade23f3cd
nkent <nkent@eecs.wsu.edu>
2017-12-19 20:35:48 -0800

    updating alexa response

--- server Scripts/alexaResponseModule.py
+++ server Scripts/alexaResponseModule.py
@@ -1,6 +1,6 @@
+“””
+Created by Nikostudios 2017
 """
-    Created by Nikostudios 2017
-    """
 
 from __future__ import print_function
 import urllib
@@ -19,7 +19,7 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
             'content': output
         },
         'shouldEndSession': should_end_session
-}
+    }
 
 
 def build_response(session_attributes, speechlet_response):
@@ -27,27 +27,22 @@ def build_response(session_attributes, speechlet_response):
         'version': '1.0',
         'sessionAttributes': session_attributes,
         'response': speechlet_response
-}
+    }
 
 
 # --------------- Functions that control the skill's behavior ------------------
 
 def test():
-    url = 'https://msghub.000webhostapp.com/logs/0.txt'
-    req = urllib.request.Request(url, None)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = str(respData)[2:]  #intent['slots']['ID']['value']
-    speech_output = speech_output.replace('\\n', ' ').replace('\n', '').replace('\\', ' ').replace('\'', '').replace('can t','cant').replace('don t', 'dont').replace('won t', 'wont')
+    speech_output = "Uh oh, Silly goose! It seems like you forgot to tell me the i.d. of the message you want me to read to you. If you want to type up your own message then checkout Shout, me, app, dot, com, or go to the apple app store and download the shout me app. Shout at you soon!"
     print("hello!")
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "Try saying, alexa ask shout me 0"
     should_end_session = True
     return build_response(session_attributes, build_speechlet_response(
-                                                                       card_title, speech_output, reprompt_text, should_end_session))
+        card_title, speech_output, reprompt_text, should_end_session))
 
 
 def get_welcome_response(intent):
@@ -64,18 +59,38 @@ def get_welcome_response(intent):
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "try saying, alexa ask shout me 0."
     should_end_session = True
+    if "DOCTYPE" in speech_output:
+        speech_output = "Uh oh, you silly chicken! It seems that you have either said a message number that doesn't exist or you have mispronounced it. If you mispronounced it try saying for example, 10 15 19 instead of 1 0 1 5 1 9. Thanks!"
     return build_response(session_attributes, build_speechlet_response(
-                                                                       card_title, speech_output, reprompt_text, should_end_session))
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_help():
+    url = 'https://msghub.000webhostapp.com/logs/0.txt'
+    req = urllib.request.Request(url, None)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = str(respData)[2:]  #intent['slots']['ID']['value']
+    speech_output = speech_output.replace('\\n', ' ').replace('\n', '').replace('\\', ' ').replace('\'', '').replace('can t','cant').replace('don t', 'dont').replace('won t', 'wont')
+    print("hello!")
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "try saying, alexa ask shout me 0."
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
 
 
 def handle_session_end_request():
     card_title = "Session Ended"
     speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-        "Have a nice day! "
+                    "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
-                                                       card_title, speech_output, None, should_end_session))
+        card_title, speech_output, None, should_end_session))
 
 
 
@@ -83,79 +98,79 @@ def handle_session_end_request():
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
-    
+
     print("on_session_started requestId=" + session_started_request['requestId']
           + ", sessionId=" + session['sessionId'])
 
 
 def on_launch(launch_request, session):
     """ Called when the user launches the skill without specifying what they
-        want
-        """
-    
+    want
+    """
+
     print("on_launch requestId=" + launch_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-          # Dispatch to your skill's launch
-    return get_welcome_response()
+    # Dispatch to your skill's launch
+    return get_help()
 
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
-    
+
     print("on_intent requestId=" + intent_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-        
-          intent = intent_request['intent']
-          intent_name = intent_request['intent']['name']
-          
-          # Dispatch to your skill's intent handlers
-          if intent_name == "ReadMessage":
-              return test()
-          elif intent_name == "MessageID":
-              return get_welcome_response(intent)
-                  elif intent_name == "AMAZON.HelpIntent":
-                      return get_welcome_response()
-                          elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-                              return handle_session_end_request()
-                                  else:
-                                      raise ValueError("Invalid intent")
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "MessageID":
+        return get_welcome_response(intent)
+    elif intent_name == "ReadMessage":
+        return test()
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_help()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
 
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
-        
-        Is not called when the skill returns should_end_session=true
-        """
+
+    Is not called when the skill returns should_end_session=true
+    """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-# add cleanup logic here
+    # add cleanup logic here
 
 
 # --------------- Main handler ------------------
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-        etc.) The JSON body of the request is provided in the event parameter.
-        """
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
-        
-          """
-              Uncomment this if statement and populate with your skill's application ID to
-              prevent someone else from configuring a skill that sends requests to this
-              function.
-              """
-          # if (event['session']['application']['applicationId'] !=
-          #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-          #     raise ValueError("Invalid Application ID")
-          
-          if event['session']['new']:
-              on_session_started({'requestId': event['request']['requestId']},
-                                 event['session'])
 
-if event['request']['type'] == "LaunchRequest":
-    return on_launch(event['request'], event['session'])
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
-elif event['request']['type'] == "SessionEndedRequest":
-    return on_session_ended(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
